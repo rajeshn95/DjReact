8fb8f76d1d03e6784fbeefd8fe47703bdbd91b45
rajeshn95 <rajesh@betalectic.com>
2021-10-12 23:54:05 +0530

    helloo

--- src/App.js
+++ src/App.js
@@ -8,7 +8,7 @@ import 'bootstrap/dist/css/bootstrap.min.css';
 import Layout from './components/Layout';
 
 class App extends Component {
-
+// learn more
   render() {
     return (
       <BrowserRouter>
